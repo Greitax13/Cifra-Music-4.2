3e2b1262d8f2c311d870185b21c5789e9afdf30b
Greitax13 <joojj1637@gmail.com>
2026-04-16 11:07:09 -0300

    Nova Versão

---
+++ Banco de Dados Cifra Music Cadrasto - 1.1.sql
@@ -0,0 +1,17 @@
+drop database cifra_music_cadastro;
+
+create database cifra_music_cadastro;
+
+use cifra_music_cadastro;
+
+CREATE TABLE tb_usuario(
+  id INT AUTO_INCREMENT,
+  nm_usuario VARCHAR(40),
+  nm_login VARCHAR(20),
+  ds_senha VARCHAR(32),
+  inadmin  ENUM("1","2"),
+  CONSTRAINT pk_usuario PRIMARY KEY(id)
+);
+
+INSERT INTO tb_usuario(nm_usuario,nm_login,ds_senha,inadmin)
+VALUES ("Administrador","admin","admin",1)
